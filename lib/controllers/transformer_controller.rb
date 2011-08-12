3bfb078a7d44b8e007f04cb6fda794b3afa16e48
Josh Moore <joshsmoore@gmail.com>
2011-08-12 17:22:01 +0800

    Rabbit controller for the transformers

---
+++ lib/controllers/transformer_controller.rb
@@ -0,0 +1,22 @@
+class TransformerController
+  def initialize(connection, transformer)
+    channel = AMQP::Channel.new(connection)
+
+    receiving_queue = channel.queue('etl.statements', :durable => true)
+    sending_queue = channel.queue('etl.transform', :durable => true)
+
+    receiving_queue.subscribe(:ack => true, &method(:receive_message))
+
+    @exchange = channel.direct('')
+    @transformer = transformer
+  end
+
+  def receive_message(payload)
+    begin
+      @transformer.process_message(BSON.deserialize(payload))
+    rescue
+      puts BSON.deserialize(payload).inspect
+    end
+
+  end
+end
